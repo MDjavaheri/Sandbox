3b39c81e02b1eae4e4d332d9fe3363b77b09da88
MDjavaheri <michael.djavaheri@mail.yu.edu>
2014-07-22 17:00:46 -0400

    Update battleship.py
    Added score keeping via a player class.
    Looking forward to the following:
    Make multiple battleships: you'll need to be careful because you need to make sure that you don’t place battleships on top of each other on the game board. You'll also want to make sure that you balance the size of the board with the number of ships so the game is still challenging and fun to play.
    
    Make battleships of different sizes: this is trickier than it sounds. All the parts of the battleship need to be vertically or horizontally touching and you’ll need to make sure you don’t accidentally place part of a ship off the side of the board.

--- battleship.py
+++ battleship.py
@@ -1,7 +1,20 @@
 __author__ = 'u4678'
 from random import randint
+
 print("How many players are we today? One (1) or two (2)?")
 players = int(input("Number of Players:"))
+
+
+class Player(object):
+    def __init__(self, score):
+        self.score = score
+    def win(self):
+        self.score += 1
+    def loss(self):
+        self.score -= 1
+P1 = Player(0)
+P2 = Player(0)
+
 if players == 1:
     board = []
     restart = True
@@ -11,28 +24,40 @@ if players == 1:
         width = int(input("Board Width:"))
         for x in range(length):
             board.append(["O"] * width)
+
         def print_board(board):
             for row in board:
                 print(" ".join(row))
+
         print("\nLet's play Battleship!")
         print_board(board)
+
         def random_row(board):
             return randint(0, len(board) - 1)
+
         def random_col(board):
-            return randint(0, len(board[0])-1)
+            return randint(0, len(board[0]) - 1)
+
         ship_row = random_row(board)
         ship_col = random_col(board)
-        # Everything from here on should go in your for loop!
-        # Be sure to indent four spaces!
+        if length*width > 25:
+            print("Today's game will feature TWO battleships, each one 2x1 in area.")
+        elif length*width > 36:
+            int(input("Would you rather Three 2x1 ships or TWO 3x1 ships? Answer 3 or 2, respectively."))
+            print("Well, we haven't gotten there yet, so make due with what you have.")
+        else:
+            print("Today's game will feature one battleship, 1x1 in area.")
+
         total_turns = int(input("How many turns do you think you'll need?"))
         turn = 0
 
         for i in range(total_turns):
             turn += 1
-            guess_row = int(input("Guess Row:"))-1
-            guess_col = int(input("Guess Col:"))-1
+            guess_row = int(input("Guess Row:")) - 1
+            guess_col = int(input("Guess Col:")) - 1
             if guess_row == ship_row and guess_col == ship_col:
                 print("Congratulations! You sunk my battleship!")
+                P1.win()
                 break
             elif (guess_row > width) or (guess_col > length):
                 print("Oops, that's not even in the ocean.")
@@ -43,15 +68,16 @@ if players == 1:
                 print("You missed my battleship!")
 
             if turn == total_turns:
-                #print("Game Over. My ship was at {0:s}, {1:s}".format(ship_row, ship_col))
+                # print("Game Over. My ship was at {0:s}, {1:s}".format(ship_row, ship_col))
                 print("Game Over. My ship was at %s, %s" % (ship_row, ship_col))
+                P1.loss()
                 break
             else:
                 print("\nTurn", turn + 1)
                 print_board(board)
         replay = str(input("Would you like to play again?"))
         if replay.lower() == 'yes':
-            #how do you restart?
+            # how do you restart?
             board = []
             print("Starting. Please wait.")
         else:
@@ -60,6 +86,7 @@ if players == 1:
 else:
     board1 = []
     board2 = []
+
     restart = True
     while restart:
         print("So you want to play? How big of a board would you like per player? Your screen size is the limit.")
@@ -68,14 +95,19 @@ else:
         for x in range(length):
             board1.append(["O"] * width)
             board2.append(["O"] * width)
+
         def print_boards(board):
             for row in board:
                 print(" ".join(row))
+
         print("Let's play Battleship!")
+
         def random_row(board):
             return randint(0, len(board) - 1)
+
         def random_col(board):
-            return randint(0, len(board[0])-1)
+            return randint(0, len(board[0]) - 1)
+
         ship_row1 = random_row(board1)
         ship_col1 = random_col(board1)
         ship_row2 = random_row(board2)
@@ -90,12 +122,14 @@ else:
             print("")
             print("Player 1, it's your turn!")
             print_boards(board1)
-            guess_row1 = int(input("Guess Row:"))-1
-            guess_col1 = int(input("Guess Col:"))-1
+            guess_row1 = int(input("Guess Row:")) - 1
+            guess_col1 = int(input("Guess Col:")) - 1
             if guess_row1 == ship_row1 and guess_col1 == ship_col1:
                 print("Congratulations! You sunk Player 2's battleship! Player 1 wins!")
                 board1[guess_row1][guess_col1] = "I"
                 print_boards(board1)
+                P1.win()
+                print("The score is:\nPlayer 1: %s\nPlayer 2: %s" % (P1.score, P2.score))
                 break
             elif (guess_row1 > width) or (guess_col1 > length):
                 print("Oops, that's not even in the ocean.")
@@ -111,12 +145,14 @@ else:
             print("Player 2, it's your turn!")
 
             print_boards(board2)
-            guess_row2 = int(input("Guess Row:"))-1
-            guess_col2 = int(input("Guess Col:"))-1
+            guess_row2 = int(input("Guess Row:")) - 1
+            guess_col2 = int(input("Guess Col:")) - 1
             if guess_row2 == ship_row2 and guess_col2 == ship_col2:
                 print("Congratulations! You sunk Player 1's battleship! Player 2 wins!")
                 board2[guess_row2][guess_col2] = "I"
                 print_boards(board2)
+                P1.win()
+                print("The score is:\nPlayer 1: %s\nPlayer 2: %s" % (P1.score, P2.score))
                 break
             elif (guess_row2 > width) or (guess_col2 > length):
                 print("Oops, that's not even in the ocean.")
@@ -129,15 +165,16 @@ else:
                 print("You missed his battleship!")
 
             if turn == total_turns:
-                #print("Game Over. My ship was at {0:s}, {1:s}".format(ship_row, ship_col))
+                # print("Game Over. My ship was at {0:s}, {1:s}".format(ship_row, ship_col))
+                P1.loss()
+                P2.loss()
                 print("No turns left. Game Over.\nNow... the battleship coordinates were:")
-                print("Player 1: %s, %s." % (ship_row1, ship_col1))
-                print("Player 2: %s, %s." % (ship_row2, ship_col2))
-
+                print("Player 1: %s, %s" % (ship_row1, ship_col1))
+                print("Player 2: %s, %s" % (ship_row2, ship_col2))
+                print("The score is:\nPlayer 1: %s\nPlayer 2: %s" % (P1.score, P2.score))
                 break
             else:
                 turn += 1
-
 
         replay = str(input("Would you like to play again?"))
         if replay.lower() == 'yes':
