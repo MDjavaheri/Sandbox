89cbfa8cf5385c0ea431871b394757439763b8f0
M Djavaheri <michael.djavaheri@mail.yu.edu>
2016-03-09 01:14:56 -0500

    implement most popular move strategy

--- RockPaperScissors Game/js/rps.js
+++ RockPaperScissors Game/js/rps.js
@@ -1,74 +1,131 @@
+/*
+Project: Rock, Paper, Scissors
+Author: MDjavaheri
+Date: March, 2016
+Version: 1.0
+
+Objective: A Rock Paper Scissors game with three different computer gameplay strategies
+Implementation: One dominant Controller class as well as a Scoreboard and Computer Opponent
+
+*/
+/* global Controller */
 $(function() {
 	"use strict";
+	
+	//The brains of the game
+	var controller = newController();
+	var scoreBoard = newScoreBoard();
+	var computer = overseasFactory();
 		
-	//Game Computer/Bot Class
-	function Factory() = {	
-		var computer {
+	//Controller: Takes GUI input and runs the game
+	function newController() {
+		var Controller = {
+			//turn counter for logging
+			turn: 1,
+			
+			play: function(humanMove) {
+				//Associative array of moves and the moves they lose to
+				var counterMove = {
+					"rock": "paper",
+					"paper": "scissors",
+					"scissors": "rock"					
+				};
+				
+				//Get computer's move
+				var compMove = computer.move();
+
+				//print standard part of log
+				$("#log").append(this.turn++ + ". Player: " + humanMove + ", Computer: " + compMove + " | ")
+				
+				
+				if (humanMove === compMove) {//It's a draw!
+					scoreBoard.tie();
+					$("#log").append("You Tied!<br>");
+				}	
+				else if (humanMove === counterMove[compMove]){ //You Win!
+					scoreBoard.win();
+					$("#log").append("You Won!<br>");
+				}
+				else { //You Lose!
+					scoreBoard.lose();
+					$("#log").append("You lost!<br>");
+				}
+				
+				//Update computer move count with most frequently successful move (counter of the user's most frequent)
+				computer.moveCount[counterMove[humanMove]]++;
+				
+				//update last move property to counter player's next move for last move strategy
+				computer.lastMoveCounter = counterMove[humanMove];
+			},		
+			//Change the computer's gameplay strategy		
+			newStrategy: function(strategy) {
+				computer.strategy = strategy;
+				$("#strategy span").text(strategy);
+			},
+			//Resart the game
+			reset: function() {
+				scoreBoard.reset();
+				computer = overseasFactory();
+				$("#log").text("");	
+				this.turn = 1;
+			}
+		}
+		return Controller;
+	};
+	
+	//Computer Opponent
+	function overseasFactory() {	
+		var computer = {
+			//counts the human's moves to see what is most frequent
 			moveCount: {
 				"rock": 0,
 				"paper": 0,
 				"scissors": 0
 			},
+			
+			//Returns the counter of the most popular player move
 			faveMove: function() {
-				//NEEDS WORK
-				Math.max(this.moveCount.rock, this.moveCount.paper, this.moveCount.scissors);
+				//uses the max value to find the corresponding key in an inverted array (Underscore.js)
+				var maxVal = Math.max(this.moveCount.rock, this.moveCount.paper, this.moveCount.scissors);
+				var invertedMoveCount = _.invert(this.moveCount);
+				return invertedMoveCount[maxVal];
 			},
-			moveSequence: new Array(),
-			strategy: "",
+			
+			//moveSequence: new Array(); a stack implementation for further implementation of various gameplay strategies
+			
+			//Determines which gameplay strategy function to call 
+			strategy: "random",
 			move: function() {
 				switch(this.strategy) {
-					case "last":
-						this.lastMove();
-						break;
-					case "fave":
-						this.faveMove();
-						break;
+					case "Last":
+						return this.lastMove();
+					case "Most Popular":
+						return this.faveMove();
 					default:
-						this.randomMove();
-						break;
+						return this.randomMove();
 				}
 			},
-			//returns the move that beats the player's last move
+			
+			//Plays move that beats the player's last move
+			lastMoveCounter: "",
 			lastMove: function() {
-				if (this.moveSequence.length > 0) {
-					return this.moveSequence.peek();
+				if (this.lastMoveCounter === "") { 
+					return this.randomMove();//defaults to a random move for the first turn 
 				}
 				else {
-					this.randomMove();//defaults to a random move for the first turn 
+					return this.lastMoveCounter;
 				}
 			},
-			//returns a random move
+			//Plays a random move
 			randomMove: function() {
 				var moves = new Array("rock", "paper", "scissors");
 				return moves[Math.round(2 * Math.random())];
 			},
-			//taunts a message based on the game results
-			taunt: function(result) {
-				var message;
-				var alertType;
-				switch(result) {
-					case "win":
-						message = "You won! I guess I went too easy on you!";
-						alertType = "alert-success";
-						break;
-					case "lose":
-						message = "You lose! I like this: let's play again!";
-						alertType = "alert-failure";
-						break;
-					case "tie":
-						message = "Draw! You're good!";
-						alertType = "alert-warning";
-						break;
-				};
-				$("<div>")
-					.addClass("alert " + alertType)
-					.data("role":"alert")
-					.text(message)
-					.appendTo("#taunt")
-			}
-			return computer;
+			
 		}
+		return computer;
 	};
+	//Records score and updates player dashboard
 	function newScoreBoard() {
 		var board = {
 			scores: {
@@ -77,72 +134,43 @@ $(function() {
 				"ties": 0
 			},
 			win: function() {
-				scores["wins"]++;
-				$("#wins").val(score.wins); 
-			}
+				$("#wins").text(++this.scores["wins"]); 
+			},
 			lose: function() {
-				scores["losses"]++;
-			}
+				$("#losses").text(++this.scores["losses"]);
+			},
 			tie: function() {
-				scores["ties"]++;
-				$("#ties").val(score.ties); 
-			}
+				$("#ties").text(++this.scores["ties"]); 
+			},
 			reset: function() {
-				$("#wins", "#ties", "#losses").val(0);
-				scores.wins = 0;
-				scores.losses = 0;
-				scores.ties = 0;
+				//resets object properties
+				this.scores["wins"] = 0;
+				this.scores["losses"] = 0;
+				this.scores["ties"] = 0;
+				
+				//reset gui
+				$("#wins").text(0); 
+				$("#ties").text(0);
+				$("#losses").text(0);
 			}
-		}
-		//resets the game scoreboard on new game
-		$("#wins", "#ties", "#losses").val(0);
+		};
 		return board;
 	};
-
-	var Controller = {
-		var scoreBoard = newScoreBoard();
-		var computer = Factory();
-		rps = {
-			"rock": "scissors",
-			"paper": "rock",
-			"scissors": "paper"
-		},
-		shoot: function(humanMove) {
-			var compMove = computer.move();
-			if (humanMove === compMove) {//It's a draw!
-				scoreBoard.tie();
-				computer.taunt("tie");
-			}	
-			else if (humanMove === rps[compMove]) {//You Win!
-				scoreboard.win();
-				computer.taunt("win");
-			}
-			else { //Try again!
-				scoreBoard.lose();
-				computer.taunt("lose"); 
-			}
-			computer.moveSequence.push(rps[playerMove]);//store the move that would've beaten the player's most recent move
-		},		
-		reset: function() {
-			scoreBoard = newScoreBoard();
-			computer = Factory();	
-		}
-	};
-	//start the game
-	var controller  newGame();
-	//jQuery methods
+	
+	//---jQuery GUI helpers---
+	//Start a new game
 	$("#reset").click(function() {
 		controller.reset();
 	});
-	
-	$("playerMoves.move").click(function(){
-		controller.shoot($(this).val);
+	//Pick a move
+	$("#playerMoves button.move").click(function(){
+		controller.play($(this).val());
+	});
+	// Change computer strategy
+	$(".strButton").click(function(){
+		var strategy = $(this).val();
+		controller.newStrategy(strategy);
 	});
 	
-	$("#strategy button").click(function(){
-		$("#strategy button").removeClass("active"); //clears the class from any previously active ones
-		$(this).setClass("active");  //adds to this one
-		var newStrategy = $(this).val();
-		Computer.strategy(newStrategy);
-	});
+	
 })
---
+++ RockPaperScissors Game/readme.md
@@ -0,0 +1,17 @@
+Rock, Paper, Scissors!
+=====
+A fun RPS (Rock, Paper, Scissors) game to play in the browser.
+Implemented primarily using regular Javascript, jQuery, and Bootstrap.
+Underscore.js used minimally for the Most Popular Move Strategy.
+Unit testing with QUnit.
+
+Three Ways To Play:
+1. Random: Let the computer randomly decide on a move
+2. Most Popular Move: The computer plays the counter to your most popular move
+3. Last Move: The computer plays the counter to your last move
+
+Considered Updates:
+1. More taunting computer messages and graphics
+2. Mac vs. PC
+3. SuperPsuedo Random Popular Move Mode: The computer picks randomly from your history of moves
+4. Less Predictable Mode: The Computer realizes it's losing to the same move in either Mode's 2 or 3 and switches to something else.
